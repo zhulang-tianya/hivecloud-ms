edfb83bc730c28ddf3c393b2f8e449a50c724300
ganrenfu <ganrf@163.com>
2026-04-27 10:19:53 +0800

    [T1-Phase1-T1.1] 完成模块层 Mapper 接口 Javadoc 注释
    - SysUserMapper: 3 个注释
    - 模块层进度：4/10（40%）
    - 总进度：23/38（61%）
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/mapper/SysUserMapper.java
+++ hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/mapper/SysUserMapper.java
@@ -4,6 +4,16 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hivecloud.system.entity.SysUser;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * 系统用户 Mapper 接口
+ * 继承 MyBatisPlus BaseMapper，获得基础 CRUD 能力
+ * 提供用户数据库访问接口
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see BaseMapper
+ * @see SysUser
+ */
 @Mapper
 public interface SysUserMapper extends BaseMapper<SysUser> {
 }
