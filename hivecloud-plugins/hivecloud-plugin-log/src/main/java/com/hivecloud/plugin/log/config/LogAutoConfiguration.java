0778da155d85e9bfe3a8a231aab29de48f7f5b00
ganrenfu <ganrf@163.com>
2026-04-27 10:22:27 +0800

    [T1-Phase1-T1.1] 完成插件层日志配置类 Javadoc 注释
    - LogAutoConfiguration: 3 个注释
    - 插件层进度：12/15（80%）
    - 总进度：28/38（74%）
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/config/LogAutoConfiguration.java
+++ hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/config/LogAutoConfiguration.java
@@ -4,6 +4,16 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * 日志模块自动配置类
+ * 扫描日志插件的组件和 Mapper 接口
+ * 实现日志模块的自动装配
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see ComponentScan
+ * @see MapperScan
+ */
 @Configuration
 @ComponentScan(basePackages = "com.hivecloud.plugin.log")
 @MapperScan(basePackages = "com.hivecloud.plugin.log.mapper")
