758eb5f3e95c6cdcdd62ef8cec8db179466c11d7
ganrenfu <ganrf@163.com>
2026-04-27 10:22:04 +0800

    [T1-Phase1-T1.1] 完成插件层日志 Mapper 接口 Javadoc 注释
    - OperLogMapper: 3 个注释
    - 插件层进度：11/15（73%）
    - 总进度：27/38（71%）
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/mapper/OperLogMapper.java
+++ hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/mapper/OperLogMapper.java
@@ -4,6 +4,16 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.hivecloud.plugin.log.entity.SysOperLog;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ * 操作日志 Mapper 接口
+ * 继承 MyBatisPlus BaseMapper，获得基础 CRUD 能力
+ * 提供操作日志数据库访问接口
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see BaseMapper
+ * @see SysOperLog
+ */
 @Mapper
 public interface OperLogMapper extends BaseMapper<SysOperLog> {
 }
