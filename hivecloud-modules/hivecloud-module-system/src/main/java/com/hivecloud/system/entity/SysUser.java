027e90a60fdd6eea78eb5143252b29654d7766f6
ganrenfu <ganrf@163.com>
2026-04-27 10:23:03 +0800

    [T1-Phase1-T1.1] 完成模块层系统用户实体类 Javadoc 注释
    - SysUser: 17 个注释
    - 模块层进度：5/10（50%）
    - 总进度：29/38（76%）
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/entity/SysUser.java
+++ hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/entity/SysUser.java
@@ -6,37 +6,85 @@ import lombok.Data;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+/**
+ * 系统用户实体类
+ * 对应数据库表 sys_user
+ * 存储用户基本信息
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see TableName
+ */
 @Data
 @TableName("sys_user")
 public class SysUser implements Serializable {
 
+    /**
+     * 序列化版本号
+     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 主键 ID，使用雪花算法生成
+     */
     @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
+    /**
+     * 用户名
+     */
     private String username;
 
+    /**
+     * 密码（加密存储）
+     */
     private String password;
 
+    /**
+     * 昵称
+     */
     private String nickname;
 
+    /**
+     * 邮箱
+     */
     private String email;
 
+    /**
+     * 手机号
+     */
     private String phone;
 
+    /**
+     * 头像 URL
+     */
     private String avatar;
 
+    /**
+     * 性别（0-未知，1-男，2-女）
+     */
     private Integer gender;
 
+    /**
+     * 状态（0-禁用，1-正常）
+     */
     private Integer status;
 
+    /**
+     * 创建时间
+     */
     @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
 
+    /**
+     * 更新时间
+     */
     @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
+    /**
+     * 逻辑删除标志（0-未删除，1-已删除）
+     */
     @TableLogic
     private Integer deleted;
 }
