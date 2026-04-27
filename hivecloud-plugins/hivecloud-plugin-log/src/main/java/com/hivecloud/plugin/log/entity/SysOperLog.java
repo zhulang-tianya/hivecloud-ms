c9878f30d171d77046591531386d6ae5ba7710af
ganrenfu <ganrf@163.com>
2026-04-27 10:20:26 +0800

    [T1-Phase1-T1.1] 完成插件层日志实体类 Javadoc 注释
    - SysOperLog: 18 个注释
    - 插件层进度：9/15（60%）
    - 总进度：24/38（63%）
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/entity/SysOperLog.java
+++ hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/entity/SysOperLog.java
@@ -7,40 +7,97 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * 系统操作日志实体类
+ * 对应数据库表 sys_oper_log
+ * 记录用户操作日志信息
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see TableName
+ */
 @Data
 @TableName("sys_oper_log")
 public class SysOperLog implements Serializable {
 
+    /**
+     * 序列化版本号
+     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 主键 ID，使用雪花算法生成
+     */
     @TableId(type = IdType.ASSIGN_ID)
     private Long id;
 
+    /**
+     * 操作标题
+     */
     private String title;
 
+    /**
+     * 业务类型
+     */
     private String businessType;
 
+    /**
+     * 方法名称
+     */
     private String method;
 
+    /**
+     * 请求方法
+     */
     private String requestMethod;
 
+    /**
+     * 操作人类别
+     */
     private String operatorType;
 
+    /**
+     * 操作人员姓名
+     */
     private String operName;
 
+    /**
+     * 请求 URL
+     */
     private String operUrl;
 
+    /**
+     * 操作 IP
+     */
     private String operIp;
 
+    /**
+     * 请求参数
+     */
     private String operParam;
 
+    /**
+     * 返回结果
+     */
     private String jsonResult;
 
+    /**
+     * 操作状态（0 正常 1 异常）
+     */
     private Integer status;
 
+    /**
+     * 错误消息
+     */
     private String errorMsg;
 
+    /**
+     * 操作时间
+     */
     private Long operTime;
 
+    /**
+     * 消耗时间
+     */
     private Long costTime;
 }
