2c6706e8bcff5045577b016ddcefff8ca3e61ccb
ganrenfu <ganrf@163.com>
2026-04-27 10:19:14 +0800

    [T1-Phase1-T1.1] 完成插件层日志注解 Javadoc 注释
    - OperLog: 6 个注释
    - 插件层进度：8/15（53%）
    - 总进度：22/38（58%）
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/annotation/OperLog.java
+++ hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/annotation/OperLog.java
@@ -2,16 +2,45 @@ package com.hivecloud.plugin.log.annotation;
 
 import java.lang.annotation.*;
 
+/**
+ * 操作日志注解
+ * 标注在 Controller 方法上，自动记录操作日志
+ * 支持配置操作标题、业务类型、是否保存请求/响应数据
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see com.hivecloud.plugin.log.aspect.OperLogAspect
+ */
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 public @interface OperLog {
 
+    /**
+     * 操作标题
+     *
+     * @return 操作标题
+     */
     String title() default "";
 
+    /**
+     * 业务类型
+     *
+     * @return 业务类型，默认为 OTHER
+     */
     String businessType() default "OTHER";
 
+    /**
+     * 是否保存请求数据
+     *
+     * @return true-保存，false-不保存
+     */
     boolean isSaveRequestData() default true;
 
+    /**
+     * 是否保存响应数据
+     *
+     * @return true-保存，false-不保存
+     */
     boolean isSaveResponseData() default false;
 }
