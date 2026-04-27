884629fe4e8d472d323821ea290ab30706954b3e
ganrenfu <ganrf@163.com>
2026-04-27 10:06:25 +0800

    [T1-Phase1-T1.1] 完成模块层控制器 Javadoc 注释
    - SysUserController: 4 个注释
    - 模块层进度：1/10
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/controller/SysUserController.java
+++ hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/controller/SysUserController.java
@@ -9,13 +9,31 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * 系统用户管理接口控制器
+ * 提供用户信息查询、更新、删除等 RESTful 接口
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see SysUserService
+ * @see UserVO
+ */
 @RestController
 @RequestMapping("/system/v1/user")
 @RequiredArgsConstructor
 public class SysUserController {
 
+    /**
+     * 系统用户服务接口
+     */
     private final SysUserService sysUserService;
 
+    /**
+     * 获取用户详细信息
+     *
+     * @param id 用户主键 ID
+     * @return 用户信息视图对象
+     */
     @GetMapping("/{id}")
     public Result<UserVO> getUserInfo(@PathVariable Long id) {
         UserVO user = sysUserService.getUserInfo(id);
