7b78541d82e0af83f38b5118627cd074640f606d
ganrenfu <ganrf@163.com>
2026-04-27 10:09:35 +0800

    [T1-Phase1-T1.1] 完成插件层日志切面 Javadoc 注释
    - OperLogAspect: 6 个注释
    - 插件层进度：3/15
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/aspect/OperLogAspect.java
+++ hivecloud-plugins/hivecloud-plugin-log/src/main/java/com/hivecloud/plugin/log/aspect/OperLogAspect.java
@@ -20,24 +20,64 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 import java.util.Collection;
 import java.util.Map;
 
+/**
+ * 操作日志切面处理器
+ * 拦截标注@OperLog 注解的方法，记录操作日志到数据库
+ * 支持正常返回和异常返回两种场景
+ * 使用 AOP 切面编程实现日志记录
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ * @see OperLog
+ * @see SysOperLog
+ * @see OperLogMapper
+ */
 @Slf4j
 @Aspect
 @Component
 @RequiredArgsConstructor
 public class OperLogAspect {
 
+    /**
+     * 操作日志 Mapper 接口，用于数据库操作
+     */
     private final OperLogMapper operLogMapper;
 
+    /**
+     * 后置返回通知
+     * 在方法正常返回后执行，记录操作日志
+     *
+     * @param joinPoint 切点信息
+     * @param controllerLog 操作日志注解
+     * @param jsonResult 方法返回值
+     */
     @AfterReturning(pointcut = "@annotation(controllerLog)", returning = "jsonResult")
     public void doAfterReturning(JoinPoint joinPoint, OperLog controllerLog, Object jsonResult) {
         handleLog(joinPoint, controllerLog, null, jsonResult);
     }
 
+    /**
+     * 后置异常通知
+     * 在方法抛出异常后执行，记录异常日志
+     *
+     * @param joinPoint 切点信息
+     * @param controllerLog 操作日志注解
+     * @param e 异常对象
+     */
     @AfterThrowing(value = "@annotation(controllerLog)", throwing = "e")
     public void doAfterThrowing(JoinPoint joinPoint, OperLog controllerLog, Exception e) {
         handleLog(joinPoint, controllerLog, e, null);
     }
 
+    /**
+     * 处理操作日志
+     * 封装操作日志信息并保存到数据库
+     *
+     * @param joinPoint 切点信息
+     * @param controllerLog 操作日志注解
+     * @param e 异常对象，正常返回时为 null
+     * @param jsonResult 方法返回值，异常时为 null
+     */
     protected void handleLog(final JoinPoint joinPoint, OperLog controllerLog, final Exception e, Object jsonResult) {
         try {
             ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
