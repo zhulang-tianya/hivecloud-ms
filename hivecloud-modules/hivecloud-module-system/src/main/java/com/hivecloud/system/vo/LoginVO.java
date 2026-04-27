906df4151127f97c562883daafbbf26cdff50359
ganrenfu <ganrf@163.com>
2026-04-27 10:29:09 +0800

    [T1-Phase1-T1.1] 完成模块层登录 VO 类 Javadoc 注释
    - LoginVO: 4 个注释
    - 模块层进度：7/10（70%）
    - 总进度：36/38（95%）
    
    Refs: #T1-OPT, #Phase1, #T1.1

--- hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/vo/LoginVO.java
+++ hivecloud-modules/hivecloud-module-system/src/main/java/com/hivecloud/system/vo/LoginVO.java
@@ -4,12 +4,28 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+/**
+ * 登录视图对象（VO）
+ * 用于封装用户登录请求参数
+ *
+ * @author HiveCloud Team
+ * @date 2026-04-25
+ */
 @Data
 public class LoginVO implements Serializable {
 
+    /**
+     * 序列化版本号
+     */
     private static final long serialVersionUID = 1L;
 
+    /**
+     * 用户名
+     */
     private String username;
 
+    /**
+     * 密码
+     */
     private String password;
 }
